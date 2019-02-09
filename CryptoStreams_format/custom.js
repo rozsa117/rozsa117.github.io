7143297bb2699faa7a6bff43fedcf1a1a0741d41
Tamas Rozsa <445653@mail.muni.cz>
2019-02-09 07:30:28 +0100

    some minor update

--- CryptoStreams_format/custom.js
+++
@@ -1,6 +0,0 @@
-var button = document.getElementById( "sidebarCollapse" );
- button.onclick = function() {
-	classList.toggle( this, 'active' );
-	classList.toggle( menuRight, 'cbp-spmenu-open' );
-	//disableOther( 'showRight' );
-};
